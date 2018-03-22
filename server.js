e3c88a2cc3eb533df637a4f056556aab88b31beb
Julien GASTINEAU <Julien GASTINEAU>
2018-03-22 01:18:11 +0100

    factorisation server.js

--- server.js
+++ server.js
@@ -49,15 +49,15 @@ io.on('connection', function(socket){
     }
     socket.on("Mouvement Y",function(data)
     {
-        deplacements = data.deplacements;
+        mouvement = data.deplacements;
         position = data.position;
-        socket.broadcast.emit('reception axe Y',{deplacements: deplacements, position:position})
+        socket.broadcast.emit('reception axe Y',{mouvement: mouvement, position:position})
     })
     socket.on("Mouvement X",function(data)
     {
-        deplacements = data.deplacements;
+        mouvement = data.deplacements;
         position = data.position;
-        socket.broadcast.emit('reception axe X',{deplacements: deplacements, position:position})
+        socket.broadcast.emit('reception axe X',{mouvement: mouvement, position:position})
     })
     // ZONE 1
     socket.on("Zone collision",function(data)
@@ -76,8 +76,8 @@ io.on('connection', function(socket){
     {
         socket.broadcast.emit('communication position',{x:data.x,y:data.y, score:data.score})
     })
-    // ZONE 2 (ZONE MOUVANTE)
-    /*socket.on("Zone2 collision",function(data)
+    // ZONE 2
+    socket.on("Zone2 collision",function(data)
     {
         if (!overload) 
         {
@@ -86,13 +86,13 @@ io.on('connection', function(socket){
             zone2Y = (Math.random()*(data.h-300))-(data.h-300)/2+data.h/2
             score = data.score + 5
             socket.emit("mise à jour de la position de la zone2",{x:zone2X, y:zone2Y, score}); 
-            setTimeout(function(){overload=false},200)
+            setTimeout(function(){overload=false},500)
         }
     })
     socket.on('transfert position zone2',function(data)
     {
         socket.broadcast.emit('communication position zone2',{x:data.x ,y:data.y, score:data.score})
-    })*/
+    })
     // RAJOUT LOAN
     socket.on('gameStarted',function()
     {
@@ -100,43 +100,21 @@ io.on('connection', function(socket){
         /*socket.broadcast.emit('you can move')*/
     })
     // BONUS
-    socket.on('createBonus',function(data)
-    {
-        if (!bonus) 
-        {
-            bonus=true;
-            bonusX = (Math.random()*(data.w-200))-(data.w-100)/2+data.w/2;
-            bonusY = (Math.random()*(data.h-150))-(data.h-75)/2+data.h/2;
-            socket.emit('created bonus',{x:bonusX,y:bonusY})
-        }
-    })
     socket.on('bonus communication',function(data)
     {
-        socket.broadcast.emit('bonus reception',{x:data.x,y:data.y})
+        socket.broadcast.emit('bonus reception',{x:data.x,y:data.y,status:data.status})
     })
     socket.on('bonus collision',function(data)
     {
-        bonus=false;
         socket.broadcast.emit('bonus removed', {totalTime:data.totalTime})
     })
     // MALUS
-    socket.on('createMalus',function(data)
-    {
-        if (!malus) 
-        {
-            malus=true;
-            malusX = (Math.random()*(data.w-200))-(data.w-100)/2+data.w/2;
-            malusY = (Math.random()*(data.h-150))-(data.h-75)/2+data.h/2;
-            socket.emit('created malus',{x:malusX,y:malusY})
-        }
-    })
     socket.on('malus communication',function(data)
     {
-        socket.broadcast.emit('malus reception',{x:data.x,y:data.y})
+        socket.broadcast.emit('malus reception',{x:data.x,y:data.y,status:data.status})
     })
     socket.on('malus collision',function(data)
     {
-        malus=false;
         socket.broadcast.emit('malus removed', {totalTime:data.totalTime})
     })
     // DANGER ZONE
